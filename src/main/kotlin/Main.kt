e3a49e81f0fe97d915e3af8ea6696e6eb2f676a0
bartieres <andre.bartieres@gmail.com>
2023-06-24 13:41:21 -0300

    Aprendendo criar IF/ When

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,34 @@
 fun main(args: Array<String>) {
     println("Seja Bem Vindo(a) ao Kotlin")
+    var titular = "André"
+    val numeroConta = 1000
+    println("Titular $titular")
+    println("Número da conta $numeroConta")
+
+    usandoIfOuWhen()
+}
+
+fun usandoIfOuWhen() {
+    var saldo = 0.0
+    saldo = 100 + 2.0
+    saldo += 200
+    saldo -= 1000
+
+    println("Saldo $saldo")
+
+    // Usando IF
+    if (saldo > 0.0) {
+        println("Conta é positiva")
+    } else if (saldo == 0.0) {
+        println("Conta é neutra")
+    } else {
+        println("Conta é negativa")
+    }
+
+    // Usando WHEN
+    when {
+        saldo > 0.0 -> println("Conta é positiva")
+        saldo == 0.0 -> println("Conta é neutra")
+        else -> println("Conta é negativa")
+    }
 }
