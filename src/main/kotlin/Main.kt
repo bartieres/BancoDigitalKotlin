4d82df40536b3040e4c2c39ab05780a1f52ec8c5
bartieres <andre.bartieres@gmail.com>
2023-06-24 14:14:04 -0300

    Aprendendo criar laco de repeticao For e While

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,22 +1,18 @@
 fun main(args: Array<String>) {
     println("Seja Bem Vindo(a) ao Kotlin")
-    var titular = "André"
-    val numeroConta = 1000
-    println("Titular $titular")
-    println("Número da conta $numeroConta")
 
-    usandoIfOuWhen()
-}
-
-fun usandoIfOuWhen() {
-    var saldo = 0.0
-    saldo = 100 + 2.0
-    saldo += 200
-    saldo -= 1000
-
+    var saldo = 10.0
     println("Saldo $saldo")
 
-    // Usando IF
+    usandoIfOuWhen(saldo)
+    usandoRepeticaoFor()
+    usandoRepeticaoWhile()
+}
+
+fun usandoIfOuWhen(saldo: Double) {
+
+    println(">>>>>>>>>> IF/ WHEN")
+
     if (saldo > 0.0) {
         println("Conta é positiva")
     } else if (saldo == 0.0) {
@@ -25,10 +21,65 @@ fun usandoIfOuWhen() {
         println("Conta é negativa")
     }
 
-    // Usando WHEN
     when {
         saldo > 0.0 -> println("Conta é positiva")
         saldo == 0.0 -> println("Conta é neutra")
         else -> println("Conta é negativa")
     }
+
+    println("<<<<<<<<<< IF/ WHEN")
+}
+
+fun usandoRepeticaoFor() {
+
+    println(">>>>>>>>>> FOR")
+
+    for (i in 1..5) {
+        var titular = "André $i"
+        val numeroConta = 1000 + i
+        var saldo = i + 10.0
+
+        println("Titular $titular")
+        println("Número da conta $numeroConta")
+        println("Saldo $saldo")
+        println("--")
+    }
+
+    println("com downTo \n")
+
+    for (i in 5 downTo 1) {
+
+        var titular = "André $i"
+        val numeroConta = 1000 + i
+        var saldo = i + 10.0
+
+        println("Titular $titular")
+        println("Número da conta $numeroConta")
+        println("Saldo $saldo")
+        println("--")
+    }
+
+    println("<<<<<<<<<< FOR")
+}
+
+fun usandoRepeticaoWhile() {
+
+    println(">>>>>>>>>> WHILE")
+
+    var i = 0
+
+    while (i < 5) {
+        var titular = "André $i"
+        val numeroConta = 1000 + i
+        var saldo = i + 10.0
+
+        println("Titular $titular")
+        println("Número da conta $numeroConta")
+        println("Saldo $saldo")
+        println("--")
+
+        i++
+    }
+
+    println("<<<<<<<<<< WHILE")
 }
