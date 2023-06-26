fc3091f4e7b461c4748dc4bf06736763910d4fa5
bartieres <andre.bartieres@gmail.com>
2023-06-26 12:04:51 -0300

    Utilizando herança

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,7 +3,7 @@ fun main(args: Array<String>) {
 
     val andre = Funcionario(
         nome = "André",
-        cpf = "01234567891",
+        cpf = "111.111.111-11",
         salario = 1000.0
     )
 
@@ -11,4 +11,42 @@ fun main(args: Array<String>) {
     println("CPF: ${andre.cpf}")
     println("Salário: ${andre.salario}")
     println("Bonificação: ${andre.bonificacao()}")
+
+    val fran = Gerente(
+        nome = "Fran",
+        cpf = "222.222.222-22",
+        salario = 2000.0,
+        senha = 1234
+    )
+
+    println("Nome: ${fran.nome}")
+    println("CPF: ${fran.cpf}")
+    println("Salário: ${fran.salario}")
+    println("Bonificação: ${fran.bonificacao()}")
+
+    if (fran.autentica(1234)) {
+        println("autenticou com sucesso")
+    } else {
+        println("falha na autenticação")
+    }
+
+    val gui = Diretor(
+        nome = "Gui",
+        cpf = "333.333.333-33",
+        salario = 4000.0,
+        senha = 4000,
+        plr = 200.0
+    )
+
+    println("Nome: ${gui.nome}")
+    println("CPF: ${gui.cpf}")
+    println("Salário: ${gui.salario}")
+    println("Bonificação: ${gui.bonificacao()}")
+    println("PLR: ${gui.plr}")
+
+    if (gui.autentica(4000)) {
+        println("autenticou com sucesso")
+    } else {
+        println("falha na autenticação")
+    }
 }
