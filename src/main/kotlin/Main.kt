4de9cf23439e761f8027abc02d4ec3f673f42bec
bartieres <andre.bartieres@gmail.com>
2023-06-26 09:31:49 -0300

    Cria classe Funcionario

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,14 @@
 fun main(args: Array<String>) {
     println("Seja Bem Vindo(a) ao Kotlin")
 
+    val andre = Funcionario(
+        nome = "André",
+        cpf = "01234567891",
+        salario = 1000.0
+    )
+
+    println("Nome: ${andre.nome}")
+    println("CPF: ${andre.cpf}")
+    println("Salário: ${andre.salario}")
+    println("Bonificação: ${andre.bonificacao()}")
 }
