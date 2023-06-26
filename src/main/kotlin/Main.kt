8a17b67bf806f9cf94ea47877b76f4b188d015d2
bartieres <andre.bartieres@gmail.com>
2023-06-25 23:29:29 -0300

    Finalizando introducao kotlin

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -7,6 +7,105 @@ fun main(args: Array<String>) {
     usandoIfOuWhen(saldo)
     usandoRepeticaoFor()
     usandoRepeticaoWhile()
+    testaCopiaEReferencias()
+
+    println("Conta André")
+    var contaAndre = Conta(titular = "André", numero = 1000)
+    contaAndre.deposita(200.0)
+    println(contaAndre.titular)
+    println(contaAndre.numero)
+    println(contaAndre.saldo)
+
+    println("Conta Fran")
+    var contaFran = Conta(numero = 1001, titular = "Fran")
+    contaFran.deposita(300.0)
+    println(contaFran.titular)
+    println(contaFran.numero)
+    println(contaFran.saldo)
+
+    println("Deposito na conta do Andre")
+    contaAndre.deposita(50.0)
+    println(contaAndre.saldo)
+
+    println("Deposito na conta da Fran")
+    contaFran.deposita(70.0)
+    println(contaFran.saldo)
+
+    println("Sacando na conta do Andre")
+    contaAndre.saca(250.0)
+    println(contaAndre.saldo)
+
+    println("Sacando na conta da Fran")
+    contaFran.saca(100.0)
+    println(contaFran.saldo)
+
+    println("saque em excesso na conta do Andre")
+    contaAndre.saca(100.0)
+    println(contaAndre.saldo)
+
+    println("saque em excesso na conta da Fran")
+    contaFran.saca(500.0)
+    println(contaFran.saldo)
+
+    println("Tranferência da conta da Fran para o Andre")
+
+    if (contaFran.transfere(300.0, contaAndre)) {
+        println("Transferência sucedida")
+    } else {
+        println("Falha na tranferência")
+    }
+
+    println(contaAndre.saldo)
+    println(contaFran.saldo)
+}
+
+class Conta(
+    var titular: String,
+    val numero: Int
+) {
+    var saldo = 0.0
+        private set
+
+    fun deposita(valor: Double) {
+        if (valor > 0) {
+            this.saldo += valor
+        }
+    }
+
+    fun saca(valor: Double) {
+        if (this.saldo >= valor) {
+            this.saldo -= valor
+        }
+    }
+
+    fun transfere(valor: Double, destino: Conta): Boolean {
+        if (this.saldo >= valor) {
+            this.saldo - +valor
+            destino.deposita(valor)
+            return true
+        }
+        return false
+    }
+}
+
+fun testaCopiaEReferencias() {
+    val numeroX = 10
+    var numeroY = numeroX
+    numeroY++
+
+    println("numeroX $numeroX")
+    println("numeroY $numeroY")
+
+    var contaJoao = Conta("João", 1002)
+    contaJoao.titular = "João"
+    var contaMaria = Conta("Maria", 1003)
+    contaMaria.titular = "Maria"
+    contaJoao.titular = "João"
+
+    println("titular conta joao: ${contaJoao.titular}")
+    println("titular conta maria: ${contaMaria.titular}")
+    println(contaJoao)
+    println(contaMaria)
 }
 
 fun usandoIfOuWhen(saldo: Double) {
